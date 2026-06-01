8ed597b9e2d12c14b1daf9f4b5938200057fecdf
wilddeath1 <57554987+wilddeath1@users.noreply.github.com>
2026-06-01 12:16:34 +0530

    Update vite.config.js
    added base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   base: '/Valorant-Esports-Scouting-Concept/',
 })
